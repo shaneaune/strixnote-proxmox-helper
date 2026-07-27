696a0370fdaa2f36d8edd42f790882e15b0a1d28
Shane Aune <shaneaune@gmail.com.com>
2026-07-26 20:32:45 -0700

    Add disk variable diagnostics

--- proxmox-create-strixnote-vm.sh
+++ proxmox-create-strixnote-vm.sh
@@ -428,6 +428,8 @@ while true; do
   break
 done
 
+declare -p DISK_GB
+
 while true; do
   read -rp "Web UI port [8080]: " WEB_PORT
   WEB_PORT="${WEB_PORT:-8080}"
@@ -455,6 +457,8 @@ GPU_DESCRIPTION=""
 if [[ "$INSTALL_MODE" == "gpu" ]]; then
   select_gpu
 fi
+
+declare -p DISK_GB
 
 # --- Password prompts ---
 
